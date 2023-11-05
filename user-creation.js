cc77ceecb1fc40a9ba7914c36e52b8703ca60c29
golovewkin <golovewkin@gmail.com>
2023-11-05 07:28:09 -0700

    add create user script

---
+++ user-creation.js
@@ -0,0 +1,12 @@
+const firebaseAdmin = require("firebase-admin");
+const createUser = async (email, password) => {
+      if (!email) throw new Error ('no email!');
+    if (!password) throw new Error ('no password!');
+
+    const firebaseUser = await firebaseAdmin.auth().createUser({
+      email: body.email,
+      password: body.password,
+    });
+
+    const userData = {firebaseId: firebaseUser.uid, ownerId: adminId, role: userRoles.user};
+}
