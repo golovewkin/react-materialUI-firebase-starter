9a273940a4a4caed711d5793819a9a63a6517225
golovewkin <golovewkin@gmail.com>
2023-11-28 21:34:12 -0700

    FormComponent new config idea

--- src/components/library-based-components/FormComponent.js
+++ src/components/library-based-components/FormComponent.js
@@ -4,11 +4,13 @@ import { validEmail, validPassword } from "../../helpers/validator.helper";
 import ButtonComponent from "./ButtonComponent/ButtonComponent";
 import { setFormState } from "../../helpers/form.helper";
 import useSubmit from "../hooks/useSubmit";
+import { FORM_INPUT_TYPES } from "../../constants/FORM_INPUT_TYPES";
 
 const FormComponent = ({
   className = "App-page__wrapper",
   sendRequest,
   configState,
+  inputs,
   children,
   resetAfterSubmit = false,
   footerButtonLabel = "Submit",
@@ -42,33 +44,38 @@ const FormComponent = ({
         submit();
       }}
     >
-      {state.email !== undefined && (
-        <TextFieldComponent
-          onChange={(value) => setFormState("email", value, setState)}
-          value={state.email}
-          type="email"
-          label="email"
-          error={!validEmail(state.email)}
-        />
-      )}
-      {state.password !== undefined && (
-        <TextFieldComponent
-          onChange={(value) => setFormState("password", value, setState)}
-          value={state.password}
-          type="password"
-          label="password"
-          error={!validPassword(state.password)}
-        />
-      )}
-      {state.name !== undefined && (
-        <TextFieldComponent
-          onChange={(value) => setFormState("name", value, setState)}
-          value={state.name}
-          type="text"
-          label="name"
-          error={!state.name}
-        />
-      )}
+      {inputs.map((input) => (
+        <>
+          {(input.type === FORM_INPUT_TYPES.email) !== undefined && (
+            <TextFieldComponent
+              onChange={(value) => setFormState("email", value, setState)}
+              value={state.email}
+              type="email"
+              label="email"
+              error={!validEmail(state.email)}
+            />
+          )}
+          {input.type === FORM_INPUT_TYPES.password && (
+            <TextFieldComponent
+              onChange={(value) => setFormState("password", value, setState)}
+              value={state.password}
+              type="password"
+              label="password"
+              error={!validPassword(state.password)}
+            />
+          )}
+          {input.type === FORM_INPUT_TYPES.text && (
+            <TextFieldComponent
+              onChange={(value) => setFormState(input.value, value, setState)}
+              value={state[input.value]}
+              type="text"
+              label={input.label}
+              error={input.isError(state[input.value])}
+            />
+          )}
+        </>
+      ))}
+
       <ButtonComponent disabled={isDisabled(state)} loading={loading}>
         {footerButtonLabel}
       </ButtonComponent>
---
+++ src/constants/FORM_INPUT_TYPES.js
@@ -0,0 +1,5 @@
+export const FORM_INPUT_TYPES = {
+  email: "email",
+  password: "password",
+  text: "text",
+};
